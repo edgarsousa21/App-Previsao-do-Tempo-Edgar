3aba1fe12edb87c9583f9d9296408213deb25f46
edgarsousa21 <edgarraphaeldesousa@hotmail.com>
2023-06-20 17:32:39 -0300

    Ajust Temperatures

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const key = "cca86614a858f699de795fc145ca5e35"
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 function insertDataScreen(date) {
+    const space = " &nbsp " 
     document.querySelector("h2.city").innerHTML = "Tempo em " + date.name
-    document.querySelector("p.temp").innerHTML = Math.floor(date.main.temp) + " °C"
+    document.querySelector("p.temp").innerHTML = `Temperatura: ${Math.floor(date.main.temp)} °C ${space} Min: ${Math.floor(date.main.temp_min)} °C ${space} Max: ${Math.ceil(date.main.temp_max)} °C `    
     document.querySelector("p.text-forecast").innerHTML = date.weather[0].description
     document.querySelector("img.img-forecast").src = `https://openweathermap.org/img/wn/${date.weather[0].icon}.png`
     document.querySelector("p.humidity").innerHTML = "Umidade: " + date.main.humidity + "%"
